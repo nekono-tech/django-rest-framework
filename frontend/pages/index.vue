28448bf39cd4d45f14221afcb1df2d3f0a22f250
nekono <krsw.freeze@gmail.com>
2024-08-31 19:57:18 +0900

    4列にしてニコニコっぽくした

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container mx-auto py-8 px-4">
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-      <div v-for="video in videos" :key="video.video_id" class="flex flex-col md:flex-row bg-white shadow-md rounded-lg overflow-hidden">
-        <div class="md:w-1/2 p-4">
+  <div class="container mx-auto py-2 px-1">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
+      <div v-for="video in videos" :key="video.video_id" class="flex flex-col lg:flex-row bg-white overflow-hidden">
+        <div class="w-full lg:w-1/2 flex flex-col p-2">
           <img
             :src="video.thumbnail_high_url"
             alt="Thumbnail"
             class="w-full aspect-w-16 aspect-h-9 object-cover"
           />
-          <p class="text-sm text-gray-500 mt-2 px-4 py-2">投稿日時: {{ video.published_at }}</p>
+          <p class="text-xs text-gray-500 mt-1">投稿日時: {{ video.published_at }}</p>
         </div>
-        <div class="md:w-1/2 p-4">
-          <h3 class="text-lg font-bold mb-2">{{ video.title }}</h3>
-          <p class="text-gray-700 text-sm">{{ truncateText(video.description, 100) }}</p>
+        <div class="flex flex-col justify-start p-2 w-full lg:w-1/2">
+          <h3 class="text-sm font-medium mb-1">{{ video.title }}</h3>
+          <client-only>
+            <p class="text-gray-700 text-xs">{{ truncateText(video.description, 60) }}</p>
+          </client-only>
         </div>
       </div>
     </div>
