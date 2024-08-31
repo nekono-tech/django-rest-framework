70974d93e8061ceb6c0a5dc90f357cac6433de0f
nekono <krsw.freeze@gmail.com>
2024-08-31 11:54:29 +0900

    APIからデータを取得できるようになった

---
+++ frontend/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="fetch">Fetch data</button>
+  </div>
+</template>
+
+<script setup>
+const url = import.meta.server ? 'http://backend:8888/api/videos/' : '/api/videos/'
+const { data, error } = useAsyncData('videos', async () => {
+  const response = await $fetch(url)
+  console.log(response)
+  return response
+})
+
+if (error.value) {
+  console.error(error.value)
+}
+
+const fetch = async () => {
+  const response = await $fetch(url)
+  console.log(response)
+}
+</script>
