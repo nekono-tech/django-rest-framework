67717e0156067b8a820f02500e32281da38bbde0
nekono <krsw.freeze@gmail.com>
2024-08-31 10:09:34 +0900

    全動画取得用のAPI作成

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
         path('example/', include('example.urls')),
+        path('videos/', include('videos.urls')),
     ]))
 ]
--- backend/videos/views.py
+++ backend/videos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Video
+from .serializers import VideoSerializer
+
+
+class IndexView(APIView):
+    def get(self, request):
+        videos = Video.objects.all()
+        serializer = VideoSerializer(videos, many=True)
+        return Response(data=serializer.data, status=HTTP_200_OK)
