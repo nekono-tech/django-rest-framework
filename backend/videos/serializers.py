cf32a682945f2085cfffa8fccb374d3e5e640635
nekono <krsw.freeze@gmail.com>
2024-08-31 19:25:20 +0900

    一覧を表示できるようにした

--- backend/videos/serializers.py
+++ backend/videos/serializers.py
@@ -4,6 +4,18 @@ from videos.models import Video
 
 
 class VideoSerializer(serializers.ModelSerializer):
+    published_at = serializers.DateTimeField(format="%Y/%m/%d %H:%M")
+
     class Meta:
         model = Video
-        fields = '__all__'
+        fields = [
+            'video_id',
+            'title',
+            'description',
+            'published_at',
+            'thumbnail_default_url',
+            'thumbnail_medium_url',
+            'thumbnail_standard_url',
+            'thumbnail_high_url',
+            'thumbnail_maxres_url'
+        ]
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,23 +1,27 @@
 <template>
-  <div>
-    <button @click="fetch">Fetch data</button>
+  <div class="container mx-auto py-8 px-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div v-for="video in videos" :key="video.video_id" class="flex flex-col md:flex-row bg-white shadow-md rounded-lg overflow-hidden">
+        <div class="md:w-1/2">
+          <img
+            :src="video.thumbnail_high_url"
+            alt="Thumbnail"
+            class="w-full aspect-w-16 aspect-h-9 object-cover"
+          />
+          <p class="text-sm text-gray-500 mt-2 px-4 py-2">投稿日時: {{ video.published_at }}</p>
+        </div>
+        <div class="md:w-1/2 p-4">
+          <h3 class="text-lg font-bold mb-2">{{ video.title }}</h3>
+          <p class="text-gray-700 text-sm">{{ video.description }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 const { $api } = useNuxtApp()
-const { data, error } = useAsyncData('videos', async () => {
-  const response = await $api.get('videos/')
-  console.log(response)
-  return response
-})
-
-if (error.value) {
-  console.error(error.value)
-}
-
-const fetch = async () => {
-  const response = await $api.get('videos/')
-  console.log(response)
-}
+const { data: videos } = await useAsyncData('videos', () =>
+    $api.get('videos/')
+)
 </script>
