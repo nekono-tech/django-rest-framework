8c53abeb420ad3a01f538b0dbdf56170263f4000
nekono <krsw.freeze@gmail.com>
2024-08-31 23:02:50 +0900

    個別動画ページを作成

--- frontend/pages/videos/[video_id].vue
+++ frontend/pages/videos/[video_id].vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container mx-auto py-4 px-2">
-    <div class="video-container">
-      <iframe
-        :src="`https://www.youtube.com/embed/${video.video_id}`"
-        allowfullscreen
-        class="w-full h-full"
-      ></iframe>
+  <div class="container mx-auto py-4 px-2 max-w-4xl">
+    <div class="w-full">
+      <div class="w-full h-full aspect-video">
+        <iframe
+          :src="`https://www.youtube.com/embed/${video.video_id}`"
+          allowfullscreen
+          class="w-full h-full"
+        ></iframe>
+      </div>
     </div>
-    <div class="mb-4">
+    <div class="mt-4">
       <h1 class="text-xl font-bold">{{ video.title }}</h1>
       <p class="text-sm text-gray-500">公開日時: {{ video.published_at }}</p>
     </div>
     <div class="mt-4">
-      <p class="text-gray-700">{{ video.description }}</p>
+      <pre class="text-gray-700">{{ video.description }}</pre>
     </div>
   </div>
 </template>
@@ -27,19 +29,3 @@ const { data: video } = await useAsyncData(`video-${videoId}`, () => {
   return $api.get(`videos/${videoId}`)
 })
 </script>
-
-<style scoped>
-.video-container {
-  position: relative;
-  width: 100%;
-  padding-bottom: 56.25%;
-  height: 0;
-}
-.video-container iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-</style>
