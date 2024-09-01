05db03b4be22f388313a7b283743522802ffc06d
nekono <krsw.freeze@gmail.com>
2024-09-01 16:37:54 +0900

    About ページを作成

---
+++ frontend/pages/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-6 mt-10 bg-white">
+    <h1 class="text-4xl font-bold mb-6">About</h1>
+    <section class="mb-6">
+      <h2 class="text-2xl font-semibold mb-2">概要</h2>
+      <p class="text-lg">
+        本サイトは、にじさんじライバーの Youtube 上の動画(メンバー限定、削除済みなど一般的にアクセスできない動画を除く)を検索できるサイトです。<br>
+        現在はテスト中のため特定のライバーの動画のみが存在しますが、今後各ライバーの動画を順次追加していく予定です。
+      </p>
+    </section>
+
+    <section class="mb-6">
+      <h2 class="text-2xl font-semibold mb-2">お問い合わせ</h2>
+      <p class="text-lg">
+        お問い合わせやご質問がある場合は、X(旧 Twitter)へご連絡ください。
+      </p>
+      <a href="https://twitter.com/iintyoyabakunai" class="text-blue-500 hover:underline">@iintyoyabakunai</a>
+    </section>
+  </div>
+</template>
