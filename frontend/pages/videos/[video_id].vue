417e66ba2a3c66f62789c884ba594a4f3e2750f5
nekono <krsw.freeze@gmail.com>
2024-09-02 22:56:46 +0900

    概要欄がはみ出るので折り返して、文字数が多い場合は折りたたむようにした

--- frontend/pages/videos/[video_id].vue
+++ frontend/pages/videos/[video_id].vue
@@ -14,7 +14,12 @@
       <p class="text-sm text-gray-500">公開日時: {{ video.published_at }}</p>
     </div>
     <div class="mt-4">
-      <pre class="text-gray-700">{{ video.description }}</pre>
+      <pre class="text-gray-700 whitespace-pre-line" :class="{ 'line-clamp-3': !showFullDescription }">
+        {{ video.description }}
+      </pre>
+      <button v-if="isLongDescription" @click="toggleDescription" class="text-blue-500 mt-2">
+        {{ showFullDescription ? '閉じる' : 'もっと見る' }}
+      </button>
     </div>
   </div>
 </template>
@@ -28,4 +33,11 @@ const videoId = route.params.video_id
 const { data: video } = await useAsyncData(`video-${videoId}`, () => {
   return $api.get(`videos/${videoId}`)
 })
+
+const showFullDescription = ref(false);
+const isLongDescription = computed(() => video.value.description.length > 200);
+
+const toggleDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
 </script>
