887cf8d8afaff8ef94eafbe150a3eccb93334f2d
nekono <krsw.freeze@gmail.com>
2024-09-05 18:39:25 +0900

    表示文字数を調整

--- frontend/components/VideoCard.vue
+++ frontend/components/VideoCard.vue
@@ -12,8 +12,8 @@
       <p class="text-xs text-gray-500 mt-1">公開日時: {{ video.published_at }}</p>
     </div>
     <div class="flex flex-col justify-start p-2 w-1/2">
-      <h3 class="text-sm font-medium mb-1">{{ truncateText(video.title, 60) }}</h3>
-      <p class="text-gray-700 text-xs">{{ truncateText(video.description, 60) }}</p>
+      <h3 class="text-sm font-medium mb-1">{{ truncateText(video.title, 40) }}</h3>
+      <p class="text-gray-700 text-xs">{{ truncateText(video.description, 40) }}</p>
     </div>
   </a>
 </template>
