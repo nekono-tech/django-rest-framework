b4062fb4055a4c65fcf8bc7cd5601b49981f12e8
nekono <krsw.freeze@gmail.com>
2024-09-05 19:13:52 +0900

    css の整理

--- frontend/components/VideoCard.vue
+++ frontend/components/VideoCard.vue
@@ -1,19 +1,19 @@
 <template>
   <a
     :href="`/videos/${video.video_id}/`"
-    class="flex flex-row bg-white overflow-hidden hover:bg-gray-300 transition-colors duration-100"
+    class="flex flex-row hover:bg-gray-300 transition-colors duration-100"
   >
-    <div class="w-1/2 flex flex-col p-2">
+    <div class="flex flex-col w-1/2 p-2">
       <img
         :src="video.thumbnail_high_url"
         alt="Thumbnail"
-        class="w-full aspect-video object-cover"
+        class="aspect-video object-cover"
       >
       <p class="text-xs text-gray-500 mt-1">公開日時: {{ video.published_at }}</p>
     </div>
-    <div class="flex flex-col justify-start p-2 w-1/2">
+    <div class="flex flex-col justify-start w-1/2 p-2">
       <h3 class="text-sm font-medium mb-1">{{ truncateText(video.title, 40) }}</h3>
-      <p class="text-gray-700 text-xs">{{ truncateText(video.description, 40) }}</p>
+      <p class="text-xs text-gray-700">{{ truncateText(video.description, 40) }}</p>
     </div>
   </a>
 </template>
