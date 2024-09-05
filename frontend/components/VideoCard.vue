3b0ef79fd42a0829fa82b87f6984b623a73e374e
nekono <krsw.freeze@gmail.com>
2024-09-05 18:28:49 +0900

    検索結果の動画リンクをコンポーネント化

---
+++ frontend/components/VideoCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    :href="`/videos/${video.video_id}/`"
+    class="flex flex-row bg-white overflow-hidden hover:bg-gray-300 transition-colors duration-100"
+  >
+    <div class="w-1/2 flex flex-col p-2">
+      <img
+        :src="video.thumbnail_high_url"
+        alt="Thumbnail"
+        class="w-full aspect-video object-cover"
+      >
+      <p class="text-xs text-gray-500 mt-1">公開日時: {{ video.published_at }}</p>
+    </div>
+    <div class="flex flex-col justify-start p-2 w-1/2">
+      <h3 class="text-sm font-medium mb-1">{{ truncateText(video.title, 60) }}</h3>
+      <p class="text-gray-700 text-xs">{{ truncateText(video.description, 60) }}</p>
+    </div>
+  </a>
+</template>
+
+<script setup>
+defineProps({
+  video: Object
+})
+
+const { truncateText } = useTextUtils()
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -63,27 +63,11 @@
 
     <!-- Videos -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
-      <a
+      <VideoCard
         v-for="video in videos.results"
-        :key="video.video_id"
-        :href="`/videos/${video.video_id}/`"
-        class="flex flex-row bg-white overflow-hidden hover:bg-gray-300 transition-colors duration-100"
-      >
-        <div class="w-1/2 flex flex-col p-2">
-          <img
-            :src="video.thumbnail_high_url"
-            alt="Thumbnail"
-            class="w-full aspect-video object-cover"
-          >
-          <p class="text-xs text-gray-500 mt-1">公開日時: {{ video.published_at }}</p>
-        </div>
-        <div class="flex flex-col justify-start p-2 w-1/2">
-          <h3 class="text-sm font-medium mb-1">{{ truncateText(video.title, 60) }}</h3>
-          <client-only>
-            <p class="text-gray-700 text-xs">{{ truncateText(video.description, 60) }}</p>
-          </client-only>
-        </div>
-      </a>
+        :key="`video-${video.video_id}`"
+        :video="video"
+      />
     </div>
 
     <pagenation
@@ -102,7 +86,6 @@
 import { ref, computed, watch, onBeforeMount } from 'vue'
 const { $api } = useNuxtApp()
 const route = useRoute()
-const { truncateText } = useTextUtils();
 
 const isLoading = ref(false)
 const livers = ref([])
