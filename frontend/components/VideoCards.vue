736608d0cef04936601c25290b3665d745013650
nekono <krsw.freeze@gmail.com>
2024-09-05 19:07:44 +0900

    動画カードの一覧をコンポーネント化

---
+++ frontend/components/VideoCards.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
+    <VideoCard
+      v-for="video in videos.results"
+      :key="`video-${video.video_id}`"
+      :video="video"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  videos: Object
+})
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -61,14 +61,7 @@
       </div>
     </div>
 
-    <!-- Videos -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
-      <VideoCard
-        v-for="video in videos.results"
-        :key="`video-${video.video_id}`"
-        :video="video"
-      />
-    </div>
+    <VideoCards :videos="videos"/>
 
     <pagenation
       :total-pages="totalPages"
